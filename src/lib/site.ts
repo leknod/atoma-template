2e6942b7a50d9d939ce7f172fde8ee850c094750
Marc <kramleknod@gmail.com>
2026-08-20 16:38:02 +0200

    feat: create base template and reusable components

---
+++ src/lib/site.ts
@@ -0,0 +1,9 @@
+export const SITE_CONFIG = {
+    name: "Nombre de la Web",
+    title: "Nombre de la Web | Eslogan",
+    description:
+        "Descripción corta para la metadescription",
+    url: "https://dominio.com",
+    locale: "es_ES",
+    ogImage: "/images/logo.png",
+};
